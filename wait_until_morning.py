80f5219a057326c5ffe64bb794a7ee75266e14c6
pikachu2508 <pikachu2508@users.noreply.github.com>
2026-07-15 07:43:56 +0800

    允许云端等待至早上九点

--- wait_until_morning.py
+++ wait_until_morning.py
@@ -4,6 +4,6 @@ from radar import now_cn
 
 target = now_cn().replace(hour=9, minute=0, second=0, microsecond=0)
 seconds = (target - now_cn()).total_seconds()
-if 0 < seconds <= 15 * 60:
+if 0 < seconds <= 2 * 60 * 60:
     print(f"距离北京时间 09:00 还有 {int(seconds)} 秒，等待后发送。")
     sleep(seconds)
